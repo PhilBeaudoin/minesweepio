0b646cbd91e4e0426144294dc001ab8d37c91a1b
Philippe Beaudoin <philippe.beaudoin@gmail.com>
2021-04-18 19:19:27 -0400

    Added algo favoring small numbers

--- src/algoTypes.js
+++ src/algoTypes.js
@@ -8,7 +8,8 @@ const ALGO = {
   BIG_EMPTY_ZONES: 6,
   LOTS_OF_SIX: 7,
   LOTS_OF_SEVEN: 8,
-  isValid: (algo) => Number.isInteger(algo) && algo >= 0 && algo <= 8
+  FAVOR_SMALL: 9,
+  isValid: (algo) => Number.isInteger(algo) && algo >= 0 && algo <= 9
 }
 
 export default ALGO;
